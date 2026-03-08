dbf6ebb467a0c758f68608e15be317a829878d80
Aythami Reyes <aythami.reyes1@gmail.com>
2026-03-08 15:28:36 +0000

    Creacion de mvc jerarquía, comienzo de nota.java

---
+++ NotasUsuario/model/Nota.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Nota {
+
+    private String titulo;
+    private String contenido;
+    private String propietario;
+
+    public Nota(String titulo, String contenido, String propietario){
+        this titulo = titulo;
+        this contenido = contenido;
+        this propietario = propietario;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public String getContenido(){
+        return contenido;
+    }
+
+    public String propietario(){
+        return propietario;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public void setContenido(String contenido){
+        this.contenido = contenido;
+    }
+
+    public void setPropietario(String propietario){
+        this.propietario = propietario;
+    }
+
+    @Override
+    public String toString(){
+        return titulo;
+    }
+
+}
